043c84f5d71fbf9f74819b7b9fe888ed0feb647f
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-15 14:07:39 -0300

    importando fontes/cores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        secundaryTitle: 'var(--secundary-title)',
+        secundarySubtitle: 'var(--secundary-subtitle)',
+        secundaryBg: 'var(--secundary-bg)',
+        mainBg: 'var(--main-bg)',
+        mainSubtitle: 'var(--main-subtitle)',
+        mainStroke: 'var(--main-stroke)',
+        mainTitle: 'var(--main-title)',
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)', 'sans-serif'],
+        alex: ['var(--font-alex)', 'cursive'],
+      },
+      fontSize: {
+        title: ["64px", "78.8px",],
+        subtitle: ["16px", "28.8px"],
+        label: ["24px", "28.8px"],
+        linkForm: ["14px", "16.8px"],
+        buttonForm: ["18px", "21.5px"],
+        placeholder: ["16px", "19.2px"]
       },
     },
   },
