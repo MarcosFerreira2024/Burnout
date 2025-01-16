a3440786d016d9354206e7ca7bf302682db1b89a
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-16 12:43:00 -0300

    colocando os botões juntos

--- src/app/components/FormButton.tsx
+++ src/app/components/FormButton.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { ButtonHTMLAttributes } from 'react'
 
-function FormButton({label}) {
+
+interface formButton extends ButtonHTMLAttributes<HTMLButtonElement>{
+  label:string,
+  props?:ButtonHTMLAttributes<HTMLButtonElement>
+}
+
+function ButtonLoginSign({label,...props}:formButton) {
+
+  
+
+
   return (
-    <button className=' self-start  focus:outline-red-600 focus:bg-secundarySubtitle focus:text-mainTitle  hover:bg-secundarySubtitle  text-secundaryTitle hover:text-mainTitle transition-all duration-300 ease-in-out text-buttonForm inner drop  w-[125px] sm:w-[200px] border-[2px] border-mainStroke py-2 font-poppins   bg-mainBg rounded-md  '>
+    <button {...props}  className=' self-start disabled:opacity-50 disabled:cursor-not-allowed focus:outline-red-600 focus:bg-secundarySubtitle focus:text-mainTitle  hover:bg-secundarySubtitle  text-secundaryTitle hover:text-mainTitle transition-all duration-300 ease-in-out text-buttonForm inner drop  w-[125px] sm:w-[200px] border-[2px] border-mainStroke py-2 font-poppins   bg-mainBg rounded-md  '>
         {label}
     </button>
   )
 }
 
-export default FormButton
+export default ButtonLoginSign
