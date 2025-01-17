8cc5145371387b49c3558180ee485436af7f9a57
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-17 19:05:31 -0300

    começo da navigation da home

---
+++ src/app/components/NavCategories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NavCategories() {
+  return (
+    <div className='mx-auto max-w-[1440px] overflow-hidden'>
+        <div className='flex max-w-[1440px] bg-red-500 gap-20  '>
+        
+
+        </div>
+    </div>
+  )
+}
+
+export default NavCategories
