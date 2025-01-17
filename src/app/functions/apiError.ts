1292ece1fc5bdd87cf2c0bc57efef41cf960cdff
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-17 19:05:46 -0300

    Update apiError.ts

--- src/app/functions/apiError.ts
+++ src/app/functions/apiError.ts
@@ -1,9 +1,6 @@
 export function ErrorMessage(e: unknown) {
     if (e instanceof Error) {
-        return {
-            ok: false,
-            error: e.message,
-            data: null
-        }
+        return e.message
+
     }
 }
