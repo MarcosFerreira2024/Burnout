8c0d9934ae958015db0291e002b2125aaeaf1d31
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-17 19:05:43 -0300

    data nav

---
+++ src/app/data/Home.ts
@@ -0,0 +1,45 @@
+import { HomeCategories } from "../Types/Interfaces/Home";
+
+export const HomeMainCategories: HomeCategories[] = [
+    {
+        ref: "/sandalias",
+        text: "Sandálias"
+    },
+    {
+        ref: "/Blusas",
+        text: "Blusas"
+    },
+    {
+        ref: "/Vestidos",
+        text: "Vestidos"
+    },
+    {
+        ref: "/Calcas",
+        text: "Calças"
+    },
+    {
+        ref: "/Bermudas",
+        text: "Bermudas"
+    },
+    {
+        ref: "/Calcinhas",
+        text: "Calcinhas"
+    },
+    {
+        ref: "/Oculos",
+        text: "Óculos"
+    },
+    {
+        ref: "/Braceletes",
+        text: "Braceletes"
+    },
+    {
+        ref: "/Chapeus",
+        text: "Chapéus"
+    },
+    {
+        ref: "/sutias",
+        text: "Sutiãs"
+    }
+]
+
