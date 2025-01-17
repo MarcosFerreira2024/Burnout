ee2ec3d5f5d7a27cc480b7c74ba043954aebcd31
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-17 19:04:02 -0300

    Update page.tsx

--- src/app/(Pages)/(root)/home/page.tsx
+++ src/app/(Pages)/(root)/home/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Carousel from '../../../components/Carousel'
 import { MainCarousel } from '../../../data/CarouselData'
+import NavCategories from '../../../components/NavCategories'
 
 function page() {
   return (
-    <div >
+    <div className='flex-1 grid mx-auto  '>
         <Carousel dados={MainCarousel} />
+
+        <NavCategories />
+
     </div>
   )
 }
