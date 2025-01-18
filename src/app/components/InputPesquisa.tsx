e8fbf015ae89b74419b5741ea59df22d3c28456f
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-18 10:39:03 -0300

    Input pesquisa navBar

---
+++ src/app/components/InputPesquisa.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Search } from '../Types/Interfaces/Input'
+
+
+
+function InputPesquisa({ref,type,placeholder,name,...props}:Search) {
+  return (
+    <div>
+        <input ref={ref} {...props} className='placeholder:text-placeholderMobile placeholder:md:text-placeholder transition-all  focus:outline-red-600 focus:bg-secundarySubtitle focus:text-mainTitle focus:placeholder:text-mainTitle hover:bg-secundarySubtitle hover:placeholder:text-mainTitle text-secundaryTitle hover:text-mainTitle duration-300 ease-in-out inner drop min-w-[100%] max-w-[400px] border-[2px] border-mainStroke p-2 font-poppins   placeholder:text-secundarySubtitle bg-mainBg rounded-md  ' type={type} name={name} placeholder={placeholder}  />
+    </div>
+  )
+}
+
+export default InputPesquisa
