5fd5e3fefd29ea184be926e5ca3799a9a9139ad7
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-18 10:39:59 -0300

    Create NavData.ts

---
+++ src/app/data/NavData.ts
@@ -0,0 +1,57 @@
+export const NavData = [
+    {
+        src: "/ui/pesquisa.svg",
+        alt: "Abrir pesquisa",
+        perfil: false,
+        label: "Pesquisa",
+        redirect: false,
+    },
+    {
+        src: "/ui/categorias.svg",
+        alt: "Ver categorias",
+        perfil: false,
+        label: "Categorias",
+        redirect: "/categorias",
+    },
+    {
+        src: "/ui/ofertas.svg",
+        alt: "Ver ofertas",
+        perfil: false,
+        label: "Ofertas",
+        redirect: "/ofertas",
+    },
+    {
+        src: "/ui/cupom.svg",
+        alt: "Ver cupons",
+        perfil: false,
+        label: "Cupons",
+        redirect: "/cupom",
+
+    },
+    {
+        src: "/ui/new.svg",
+        alt: "Abrir novidades",
+        perfil: false,
+        label: "Novidades",
+        redirect: "/novidades",
+
+    },
+    {
+        src: "/ui/cart.svg",
+        alt: "Abrir carrinho",
+        perfil: false,
+        label: "Carrinho",
+        redirect: "/carrinho",
+
+    },
+    {
+        src: "/ui/perfil.png",
+        alt: "Abrir perfil",
+        perfil: true,
+        label: "Perfil",
+        redirect: "/perfil",
+
+    },
+
+
+]
