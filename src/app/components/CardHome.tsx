782d74a87a2aa7fdb60ee202e5b9faceff12e910
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-19 17:49:03 -0300

    começo cardHome

---
+++ src/app/components/CardHome.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function CardHome() {
+  return (
+    <div className='max-w-[1440px] mx-auto w-full relative mt-[116px] mb-[120px] '>
+        <div className=' w-[100%] h-[100%] flex justify-center '>
+            <div className='xl:max-w-[600px] md:max-w-[450px] max-w-[300px] xl:min-h-[600px] md:min-h-[450px] min-h-[300px] max-h-[300px] md:max-h-[450px] xl:max-h-[600px] w-full h-full rounded-full  bg-card globalShadow'></div>
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default CardHome
