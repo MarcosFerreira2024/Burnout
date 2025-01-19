ad85e61317eacac552d2a11628335ed1c1fd2002
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-19 17:48:50 -0300

    Update NavBarMobile.tsx

--- src/app/components/NavBarMobile.tsx
+++ src/app/components/NavBarMobile.tsx
@@ -9,7 +9,7 @@ import NavButton from './NavButtons'
 function NavBarMobile() {
   return (
     <>
-        <aside className='items-center globalShadow fixed gap-2 h-[60px] top-0 w-[100%] bg-mainBg    flex  justify-between p-2  '>
+        <aside className='items-center globalShadow fixed gap-2 h-[60px] top-0 w-[100%] bg-mainBg  z-[999]   flex  justify-between p-2  '>
             <Image onClick={()=> redirect("/home")} src={"/auth/logo-burnout.svg"} alt='logo burnout' width={120} height={25} />
             <div className='flex globalShadow hover:bg-mainStroke ease-in-out duration-300 opacity-90 transition-all items-center relative  p-2 rounded-md bg-mainBg font-poppins w-[100%] max-w-[300px]  border-mainStroke border-[1px]    outline-none'>
                 <label htmlFor="search" className='flex items-center  cursor-pointer'><Image alt='' src={"/ui/pesquisa.svg"} width={16} className='absolute right-2' height={16} /></label>
@@ -19,7 +19,7 @@ function NavBarMobile() {
                 <NavButton href='/perfil' alt='perfil' label='perfil' src='/ui/perfil.png' perfil />
             </div>
         </aside>
-        <main className='px-5 globalShadow h-[60px] py-5 fixed bottom-0 w-[100%] bg-mainBg     '>
+        <main className='px-5 z-[999] globalShadow h-[60px] py-5 fixed bottom-0 w-[100%] bg-mainBg     '>
         
 
         </main>
