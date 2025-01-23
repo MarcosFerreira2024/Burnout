eca8b492695b64d470b3c05599ed1e295b45f315
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-23 15:36:59 -0300

    botao genérico

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -4,16 +4,17 @@ import React, { ButtonHTMLAttributes } from 'react'
 interface formButton extends ButtonHTMLAttributes<HTMLButtonElement>{
   classes?:string
   label:string,
+  disabled?:boolean,
   props?:ButtonHTMLAttributes<HTMLButtonElement>
 }
 
-function Button({label,classes, ...props}:formButton) {
+function Button({label,disabled,classes, ...props}:formButton) {
 
   
 
 
   return (
-    <button {...props}  className={` ${classes}   globalShadow w-[1000px] self-start disabled:opacity-50 disabled:cursor-not-allowed focus:outline-red-600 focus:bg-secundarySubtitle focus:text-mainTitle  hover:bg-secundarySubtitle  text-secundaryTitle hover:text-mainTitle transition-all duration-300 ease-in-out text-buttonForm     border-[2px] border-mainStroke py-2 font-poppins   bg-mainBg rounded-md  `}>
+    <button {...props} disabled={disabled}  className={` ${classes}    globalShadow  self-start disabled:opacity-50 disabled:cursor-not-allowed focus:outline-red-600 focus:bg-secundarySubtitle focus:text-mainTitle  hover:bg-secundarySubtitle  text-secundaryTitle hover:text-mainTitle transition-all duration-300 ease-in-out text-buttonForm     border-[2px] border-mainStroke py-2 font-poppins   bg-mainBg rounded-md  `}>
         {label}
     </button>
   )
