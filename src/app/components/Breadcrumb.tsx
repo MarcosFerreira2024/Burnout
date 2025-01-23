db7b5de5e50ae86d49e0d8de91b70e0fb1f8e62e
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-23 15:36:41 -0300

    breadcrumb

---
+++ src/app/components/Breadcrumb.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import React from 'react'
+
+function Breadcrumb({segments}:{segments:string[]}) {
+  return (
+        <nav className='text-sm font-poppins'>
+            <ul className='flex'>
+                {segments.map((segment, index) => {
+                const path = `/${segments.slice(0, index + 1).join('/')}`;
+                return (
+                    <li key={index}>
+                        <Link  href={path} className='flex focus:outline-red-600 text-mainBg'  >
+                            <p className='hover:underline hover:opacity-90   '>{segment.charAt(0).toUpperCase() + segment.slice(1)}</p>
+                            <span>/</span>
+                        </Link>
+                    </li>
+
+                    
+                );
+                })}
+            </ul>
+
+        </nav>
+  )
+}
+
+export default Breadcrumb
