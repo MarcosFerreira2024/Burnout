ed24eb26d6b8367b68606508d10b9f3319e689a1
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-23 15:36:51 -0300

    modal de aviso de compra

---
+++ src/app/components/AvisoModal.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import React, { useContext } from 'react'
+import { ComprasContext } from '../Contexts/ComprasContext'
+
+
+
+function AvisoModal({produto}:{produto:string}) {
+    const {aviso} = useContext(ComprasContext)
+  return (
+    <div className={`${aviso?"translate-y-0":"translate-y-[200px]"} absolute bottom-20   md:bottom-5 right-5 transition-all duration-300 ease-in-out  font-poppins  rounded-lg globalShadow  border-[2px] border-mainStroke`}>
+      <div className='flex flex-col gap-2  p-2 text-mainTitle'>
+        <h2 className='text-lg'>Produto : <span>{produto}</span></h2>
+
+        <p className='text-mainSubtitle text-sm'>Foi adicionado ao Carrinho</p>
+        
+    </div>
+
+    </div>
+  )
+}
+
+export default AvisoModal
