7128dae8a7856e85f6e4502799080d3cca75ba8c
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-24 18:32:31 -0300

    Create PerfilData.ts

---
+++ src/app/data/PerfilData.ts
@@ -0,0 +1,39 @@
+export const Options = [
+    {
+        title: "Meus Pedidos",
+        subtitle: "Veja seus pedidos",
+        redirect: "/pedidos",
+        icon: "ShoppingCart",
+
+    },
+    {
+        title: "Favoritos",
+        subtitle: "Veja seus favoritos",
+        redirect: false,
+        icon: "Heart",
+    },
+    {
+        title: "Dados",
+        subtitle: "Veja ou altere seus dados",
+        redirect: false,
+        icon: "User",
+    },
+    {
+        title: "Avaliações",
+        subtitle: "Veja suas avaliações",
+        redirect: false,
+        icon: "Star",
+    },
+    {
+        title: "Atendimento",
+        subtitle: "Veja seu histórico de atendimento",
+        redirect: "/atendimento",
+        icon: "Headset",
+    },
+    {
+        title: "Carteira",
+        subtitle: "Veja seus cartões cadastrados",
+        redirect: false,
+        icon: "CreditCard",
+    }
+]
