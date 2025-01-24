81be3c16113c9637987f5a2082ffb65955dee081
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-24 18:33:37 -0300

    importações

--- src/app/(Pages)/(root)/perfil/page.tsx
+++ src/app/(Pages)/(root)/perfil/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import HeaderPerfil from '../../../components/HeaderPerfil'
+import OptionsPerfil from '../../../components/OptionsPerfil'
+import Footer from '../../../components/Footer'
 
 function page() {
   return (
     <div className='flex-1 grid mx-auto mt-20 sm:mt-0 px-5  '>
       <HeaderPerfil />
+      <OptionsPerfil />
+      <Footer/>
+
     </div>
   )
 }
