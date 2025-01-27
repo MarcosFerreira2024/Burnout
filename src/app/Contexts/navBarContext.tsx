75e05a7587dfe417360d1f012e099915ba11add4
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-27 18:46:01 -0300

    sistema de pesquisa

--- src/app/Contexts/navBarContext.tsx
+++ src/app/Contexts/navBarContext.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { createContext, useState } from 'react';
+import { redirect } from 'next/navigation';
+import { createContext, useEffect, useState } from 'react';
 
 type ContextProviderProps = {
     children: React.ReactNode;
@@ -8,15 +9,21 @@ type ContextProviderProps = {
 
 type NavBarContext = {
     modais: boolean,
-    setModais: React.Dispatch<React.SetStateAction<boolean>>,
     modalPesquisa: boolean,
-    showModalPesquisa: React.Dispatch<React.SetStateAction<boolean>>,
     modalCategories: boolean,
-    showModalCategories: React.Dispatch<React.SetStateAction<boolean>>,
     mounted: boolean,
-    setMounted: React.Dispatch<React.SetStateAction<boolean>>,
     anima:boolean,
-    setAnima:React.Dispatch<React.SetStateAction<boolean>>
+    searchValue:string,
+    pesquisas:string[],
+    setModais: React.Dispatch<React.SetStateAction<boolean>>,
+    showModalPesquisa: React.Dispatch<React.SetStateAction<boolean>>,
+    showModalCategories: React.Dispatch<React.SetStateAction<boolean>>,
+    setMounted: React.Dispatch<React.SetStateAction<boolean>>,
+    setAnima:React.Dispatch<React.SetStateAction<boolean>>,
+    setPesquisas:React.Dispatch<React.SetStateAction<string[]>>,
+    setSearchValue:React.Dispatch<React.SetStateAction<string>>,
+    handleSearch:(e:React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLImageElement>)=>void,
+    handleDeleteSearch:(e:React.MouseEvent<SVGSVGElement>)=>void
     
 };
 
@@ -31,20 +38,80 @@ export const NavContextProvider = ({ children }: ContextProviderProps) => {
     const [mounted,setMounted] = useState(false)
     const [anima,setAnima] = useState(false)
 
+    const [searchValue,setSearchValue] = useState('')
+
+    const [pesquisas, setPesquisas] = useState<string[]>([])
+    
+
+        function handleSearch (e:React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLImageElement>){
+          e.preventDefault()
+          if(searchValue !== ''){
+            setModais(false)
+            showModalPesquisa(false)
+            if(!(pesquisas.includes(searchValue))){
+              localStorage.setItem('search',searchValue)
+              setPesquisas([...pesquisas,searchValue])
+              
+            }
+            redirect(`/home/${searchValue}`)
+          }
+          return
+            
+    
+    
+        }
+        useEffect(() => {
+            if(localStorage.getItem('search') ){
+                if(localStorage.getItem('search') === ""){
+                    localStorage.removeItem("search")
+                    return
+                }
+                const values = localStorage.getItem('search').split(" ")
+            
+                setPesquisas(values)
+                return
+                
+            }
+
+              
+        
+            
+            },[setPesquisas] )
+
+        const handleDeleteSearch = (e:React.MouseEvent<SVGSVGElement>)=>{
+            e.preventDefault()
+            const updated =pesquisas.filter((item) => {
+              if(item !== e.currentTarget.previousElementSibling?.textContent){
+                return item
+              }
+             })
+              setPesquisas(updated)
+              localStorage.setItem('search',updated.join(" "))
+            
+      
+          }
+
 
     return (
         <NavContext.Provider
             value={{
+                pesquisas,
                 modais,
-                setModais,
                 modalPesquisa,
                 modalCategories,
                 mounted,
                 anima,
+                searchValue,
+                setPesquisas,
+                setSearchValue,
                 showModalPesquisa,
                 showModalCategories,
                 setMounted,
                 setAnima,
+                setModais,
+                handleDeleteSearch,
+                handleSearch
+
                 
             }}
         >
