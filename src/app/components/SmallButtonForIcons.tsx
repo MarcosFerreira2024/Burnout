1ce4f0bc30dd981cca3bdd5d682917a25e69de55
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-02-02 19:18:50 -0300

    Update SmallButtonForIcons.tsx

--- src/app/components/SmallButtonForIcons.tsx
+++ src/app/components/SmallButtonForIcons.tsx
@@ -8,12 +8,14 @@ type SmallButton = {
     width?:number,
     height?:number,
     classes?:string,
+    title:string
     props?:React.ComponentPropsWithoutRef<"button">,
+    onClick: () => void
 }
 
-function SmallButtonForIcons({src,classes,alt,width,height,...props}:SmallButton) {
+function SmallButtonForIcons({src,onClick,classes,alt,width,title,height,...props}:SmallButton) {
   return (
-    <button {...props} title='Trocar foto de perfil' className={`${classes} w-6 group-hover:scale-125 group-hover:border-white ease-in-out duration-300 trnasition-colors h-6 absolute  rounded-full z-10 bottom-0 right-0 items-center flex justify-center bg-mainBg border-[1px] border-mainStroke globalShadow`}>
+    <button onClick={onClick} {...props} title={title} className={`${classes} w-6 group-hover:scale-125 group-hover:border-white ease-in-out duration-300 trnasition-colors h-6 absolute  rounded-full z-10 bottom-0 right-0 items-center flex justify-center bg-mainBg border-[1px] border-mainStroke globalShadow`}>
         {src?<Image src={src} width={width?width:16} height={height?height:16} alt={alt} title={alt}/>:<LucideCamera  width={16} height={16} className='text-white'/>}
       
     </button>
