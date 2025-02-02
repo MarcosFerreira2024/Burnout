6901d04716c9896c511df454a7a86d34a985e6c0
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-02-02 19:20:29 -0300

    trocando foto do usuário

--- src/app/components/HeaderPerfil.tsx
+++ src/app/components/HeaderPerfil.tsx
@@ -7,9 +7,12 @@ import { LucideHeart, LucideMail, LucideShoppingCart } from 'lucide-react'
 import ProfileButton from './ProfileButton'
 
 function HeaderPerfil() {
-    const {user} = useContext(UserContext)
+    const {user,cart,setModalPicture} = useContext(UserContext)
+
+
+
   return (
-    <div className=" w-[100%] sm:mt-5">
+    <div className=" w-[100%]  sm:mt-5">
       <div  className='mx-auto  max-w-[1440px]    '>
 
         <div className='w-full py-5   px-5 items-center rounded-md bg-mainBg flex globalShadow'>
@@ -17,7 +20,7 @@ function HeaderPerfil() {
                 <div className='flex gap-2'>
                     <div className='relative group  max-h-[80px] max-w-[80px] min-h-[80px] min-w-[80px] md:max-h-[98px] md:max-w-[98px] md:min-h-[98px] md:min-w-[98px]'>
                         <Image title={user.name} alt={"foto de " + user.name} src={user.photo?user.photo:"/ui/perfil.png"} width={112} height={112} className='w-[80px] h-[80px] md:w-[98px] md:h-[98px] globalShadow   duration-300 ease-in-out transition-colors  object-center rounded-full border-2 border-mainStroke object-cover' />
-                        <SmallButtonForIcons />
+                        <SmallButtonForIcons title={"Trocar foto de Perfil"} onClick={()=> setModalPicture(true)} />
                     </div>
                     <div className='flex flex-col gap-1'>
                         <h1 className='text-perfilTitleMobile md:text-perfilTitle text-nowrap'>{user.name.split(" ").length>1?user.name.split(" ")[0] +" "+ user.name.split(" ")[user.name.split(" ").length-1]:user.name}</h1>
@@ -33,7 +36,7 @@ function HeaderPerfil() {
                         </div>
                         <div className='flex gap-2 items-center '>
                             <LucideShoppingCart width={16} height={16}/>
-                            <p className='md:text-perfilSubtitle text-perfilSubtitleMobile'>{user.carrinho?(user.carrinho.cartItem.length as number === 1 ? "1 produto no carrinho" :user.carrinho.cartItem.length + " produtos no carrinho"):"0 produtos no carrinho"} </p>
+                            <p className='md:text-perfilSubtitle text-perfilSubtitleMobile'>{cart && cart.length?(cart.length as number === 1 ? "1 produto no carrinho" :cart.length + " produtos no carrinho"):"0 produtos no carrinho"} </p>
 
                         </div>
                     </div>
