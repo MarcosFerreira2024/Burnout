f66499eb485d7891f3f3abdf8f036e39fab51528
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-02-11 04:21:27 -0300

    group-hover/image

--- src/app/components/SmallButtonForIcons.tsx
+++ src/app/components/SmallButtonForIcons.tsx
@@ -15,7 +15,7 @@ type SmallButton = {
 
 function SmallButtonForIcons({src,onClick,classes,alt,width,title,height,...props}:SmallButton) {
   return (
-    <button onClick={onClick} {...props} title={title} className={`${classes} w-6 group-hover:scale-125 group-hover:border-white ease-in-out duration-300 trnasition-colors h-6 absolute  rounded-full z-10 bottom-0 right-0 items-center flex justify-center bg-mainBg border-[1px] border-mainStroke globalShadow`}>
+    <button onClick={onClick} {...props} title={title} className={`${classes} w-6 group-hover/image:scale-125 group-hover/image:border-white ease-in-out duration-300 trnasition-colors h-6 absolute  rounded-full z-10 bottom-0 right-0 items-center flex justify-center bg-mainBg border-[1px] border-mainStroke globalShadow`}>
         {src?<Image src={src} width={width?width:16} height={height?height:16} alt={alt} title={alt}/>:<LucideCamera  width={16} height={16} className='text-white'/>}
       
     </button>
