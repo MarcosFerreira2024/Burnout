77f7a0e605b32963700a3f9677b4cb3c613d8410
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-02-11 18:42:56 -0300

    Update CarouselData.ts

--- src/app/data/CarouselData.ts
+++ src/app/data/CarouselData.ts
@@ -3,19 +3,19 @@ export const MainCarousel = [
         src: "/home/hollywoodStyle.png",
         alt: "Estilo de Hollywood",
         texto: 'Hollywood Style',
-        redirect: "/home/style/hollywood"
+        redirect: "/home/style/hollywood/1"
     },
     {
         src: "/home/streetStyle.png",
         alt: "Estilo das ruas",
         texto: 'Street Style',
-        redirect: "/home/style/street"
+        redirect: "/home/style/street/1"
     },
     {
         src: "/home/popStyle.png",
         alt: "Estilo pop",
         texto: 'Pop Style',
-        redirect: "/home/style/pop"
+        redirect: "/home/style/pop/1"
     }
 
 ]
@@ -25,25 +25,25 @@ export const Estacoes = [
         src: "/home/verão.jpg",
         alt: "Estilo de verão",
         texto: 'Verão',
-        redirect: "/home/style/verao"
+        redirect: "/home/style/verao/1"
     },
     {
         src: "/home/outono.jpg",
         alt: "Estilo de outono",
         texto: 'Outono',
-        redirect: "/home/style/outono"
+        redirect: "/home/style/outono/1"
     },
     {
         src: "/home/inverno.jpg",
         alt: "Estilo de inverno",
         texto: 'Inverno',
-        redirect: "/home/style/inverno"
+        redirect: "/home/style/inverno/1"
     },
     {
         src: "/home/primavera.jpg",
         alt: "Estilo de primavera",
         texto: 'Primavera',
-        redirect: "/home/style/primavera"
+        redirect: "/home/style/primavera/1"
     },
 
 
